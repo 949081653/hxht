1e4d365f30bb69c62e8c7657cdd6693fa236e799
949081653 <949081653@qq.com>
2018-10-12 15:52:26 +0800

    first commit

---
+++ hxht-manager/src/main/java/com/gx/HxhtManagerApplication.java
@@ -0,0 +1,12 @@
+package com.gx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HxhtManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HxhtManagerApplication.class, args);
+	}
+}
---
+++ hxht-manager/src/test/java/com/gx/HxhtManagerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gx;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HxhtManagerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
